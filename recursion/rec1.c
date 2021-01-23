9b06f7f5fc17f1adc3ed3ef0f33bab556b7d4ca4
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-01-23 16:15:29 +0530

    Update rec1.c

--- recursion/rec1.c
+++ recursion/rec1.c
@@ -1,1 +1,23 @@
+void show(int x)
+{
+  if(x==0)return;
+  printf("%d\n",x);
+  show(x-1);
+  printf("ram\n");
+}
+void main()
+{
+  show(5);
+}
+
+//5
+//4
+//3
+//2
+//1
+//ram
+//ram
+//ram
+//ram
+//ram
 
