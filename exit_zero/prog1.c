b3c8350da7ca035a6cd7de7692056605f892309b
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-01-30 23:29:38 +0530

    Update prog1.c

--- exit_zero/prog1.c
+++ exit_zero/prog1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+void show()
+{
+  printf("show 1\n");
+  exit(0);
+  printf("show 2\n");
+}
+void main()
+{
+  printf("main 1\n");
+  show();
+  printf("main 2\n");
+}
 
+//main 1
+//show 1
