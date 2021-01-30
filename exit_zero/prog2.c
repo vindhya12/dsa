d400b39a9eea7af88717322c641200d50d72f2fd
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-01-30 23:32:44 +0530

    Update prog2.c

--- exit_zero/prog2.c
+++ exit_zero/prog2.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+void show(int n)
+{
+  if(i==5)exit(0);
+  printf("%d\n",i);
+}
+void main()
+{
+  int i;
+  for(i=1;i<=10;i++)
+  {
+    show(i);
+  }
+}
 
+//1
+//2
+//3
+//4
+  
