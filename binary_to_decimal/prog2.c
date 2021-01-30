01d6421b33423037fdafc893f46f82a6b651df93
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-01-31 00:05:50 +0530

    Update prog2.c

--- binary_to_decimal/prog2.c
+++ binary_to_decimal/prog2.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+void main()
+{
+  int n=32,s=1,s1=0;
+  int r,n1;
+  while(n!=0)
+  {
+    r=n%2;
+    s=s*10+r;
+    n=n/2;
+  }
+  while(s!=1)
+  {
+    n1=s%10;
+    s1=s1*10+n1;
+    s=s/2;
+  }
+  printf("%d",s1);
+}
 
+// 000001
+    
