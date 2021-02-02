7f60d12ea48cc29bdacbf64a5fca3ebfaee4014c
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-03 00:35:00 +0530

    Update prog4.c

--- array/prog4.c
+++ array/prog4.c
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int getSum(int a[],int n);
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  {
+      int a[n];
+      for(int i=0;i<n;i++)
+      {
+        cin >> a[i];
+      }
+      cout << getSum(a,n) << end1;
+  }
+
+return 0;
+}
+
+int getSum(int a[], int n) {
+    // Your code goes here
+    int i;
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    return sum;
+}   
 
