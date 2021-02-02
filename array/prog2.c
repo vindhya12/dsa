aea65aed1c0cc0edfa0cf6742e5e9f69f23ec512
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-03 00:20:58 +0530

    Update prog2.c

--- array/prog2.c
+++ array/prog2.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+void show(int a[])
+{
+  a[2]=55;
+}
+void main()
+{
+  int i;
+  int x[]={10,20,30,40,50};
+  show(x);
+  for(i=0;i<5;i++)
+  {
+    printf("%d, ",x[i]);
+  }
+}
 
+//10, 20, 55, 40, 50
