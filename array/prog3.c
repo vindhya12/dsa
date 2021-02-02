869773f51e103db0414b64778a3fa0073ae51499
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-03 00:27:48 +0530

    Update prog3.c

--- array/prog3.c
+++ array/prog3.c
@@ -1,1 +1,41 @@
+// Largest Element in Array 
+
+#include <stdio.h>
+int largest(int arr[],int n);
+
+int main()
+{
+  int arr[100];
+  int i,t,n;
+  scanf("%d",&n);
+  while(t__)
+  {
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    printf("%d",largest(arr,n));
+    printf("\n");
+  }
+  return 0;
+  
+}
+
+// C function to find maximum in arr[] of size n
+int largest(int arr[], int n) 
+{
+    int i;
+
+    // Initialize maximum element
+    int max = arr[0];
+
+    // Traverse array elements from second and
+    // compare every element with current max
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
 
