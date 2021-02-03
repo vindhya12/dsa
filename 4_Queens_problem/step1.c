8b258d794a12ba870c4ae014ec96f63682fc618d
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-03 22:52:31 +0530

    Update step1.c

--- 4_Queens_problem/step1.c
+++ 4_Queens_problem/step1.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+bool isvalid(int x[4][4],int r,int c)
+{
+  int i,j;
+  //row check
+  for(j=c;j>=0;j--)
+  {
+    if(x[r][j])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+void main()
+{
+  int i;
+  int x[4][4]={
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0}
+  };
+  if(isvalid(x,0,3))
+  {
+    x[0][3]=1;
+  }
+  for(i=0;i<4;i++)
+  {
+    for(j=0;j<4;j++)
+    {
+      printf("%d",x[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+// 0 0 0 1
+// 0 0 0 0
+// 0 0 0 0
+// 0 0 0 0
+
+    
+    
