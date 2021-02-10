43c1699e9ed24c2552ef4cd03e5957dfd3e3d5b7
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 22:24:55 +0530

    Update prog7.c

--- pointers/prog7.c
+++ pointers/prog7.c
@@ -1,1 +1,16 @@
+//Print a list using pointer
 
+#include <stdio.h>
+void main()
+{
+  int i;
+  int x[]={1,2,3,4,5};
+  int *p=&x[0];
+  for(i=0;i<5;i++)
+  {
+    printf("%d ",*p);
+  }
+}
+
+//1 2 3 4 5
+  
