a4a8f2ea40ba27969f722050d0d28e5c1a0b3db4
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 22:20:56 +0530

    Update prog5.c

--- pointers/prog5.c
+++ pointers/prog5.c
@@ -1,1 +1,13 @@
+// to show that data stored in array has continuous memory location
 
+#include <stdio.h>
+void main()
+{
+  int x[]={1,2,3};
+  for(i=0;i<3;i++)
+  {
+    printf("%d",&x[i]);
+  }
+}
+
+//6422288  6422292  6422296
