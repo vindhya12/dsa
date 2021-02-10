c0e18d1977887bcd2d48fcf8ee3e36abe7227a17
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 10:44:11 +0530

    Update prog3.c

--- pointers/prog3.c
+++ pointers/prog3.c
@@ -1,1 +1,23 @@
+// Showing the sizeof the given data types
 
+#include <stdio.h>
+void main()
+{
+  int x=10;
+  float y=10.0;
+  long x1=10;
+  double y1=10.0;
+  char z=77;
+  
+  printf("int=%d\n",sizeof(x));
+  printf("long=%d\n",sizeof(x1));
+  printf("float=%d\n",sizeof(y));
+  printf("double=%d\n",sizeof(y1));
+  printf("char=%d\n",sizeof(z));
+}
+
+//int =4
+//long=4
+//float=4
+//double=8
+//char=1
