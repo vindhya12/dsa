745ce0d358d6345d1fe5c3609239af575b2575eb
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 10:48:06 +0530

    Update prog4.c

--- pointers/prog4.c
+++ pointers/prog4.c
@@ -1,1 +1,15 @@
+// Showing the size of array
 
+#include <stdio.h>
+void main()
+{
+  int x[]={1,2,3,4,5,6,7,8,9,0};
+  //int x[10];
+  
+  printf("Size of the array x=%d\n",sizeof(x));
+  printf("Size of x[0]=%d\n",sizeof(x[0]);
+}
+         
+         
+ // Size of the array x=40
+         // Size of x[0]=4
