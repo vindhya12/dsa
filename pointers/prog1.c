a299e2470c1c5bcd7e8b1380f4ac6e77a97de85b
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 10:24:26 +0530

    Update prog1.c

--- pointers/prog1.c
+++ pointers/prog1.c
@@ -2,5 +2,20 @@
 #include <stdio.h>
 void main()
 {
-  
+  int x=0;
+  //int *p=&x;
+  int *p;
+  p=&x;
+  printf("x=%d\n",x);
+  printf("&x=%d\n",&x);
+  printf("p=%d\n",p);
+  printf("*p=%d\n",*p);
+  printf("&p=%d\n",&p);
+}
 
+
+//x=10
+//&x=6422300
+//p=6422300
+//*p=10
+//&p=6422296
