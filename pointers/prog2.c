c6f1208cc83511e4bc44f0f2cb95cbb7d5534615
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-02-10 10:28:40 +0530

    Update prog2.c

--- pointers/prog2.c
+++ pointers/prog2.c
@@ -1,1 +1,21 @@
+// Program to change values using pointer
 
+#include <stdio.h>
+void main()
+{
+  int x=10;
+  int *p=&x;
+  printf("x=%d\n",x);
+  printf("*p=%d\n\n",*p);
+  
+  *p=20;
+  printf("x=%d\n",x);
+  printf("*p=%d\n",*p);
+}
+
+// x=10
+//*p=10
+
+//x=20
+//*p=20
+  
