2153a24417ba0685161d292e7d52cb8a4627b3d0
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-03-23 16:12:46 +0530

    Update Selection_Sort.java

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -1,1 +1,31 @@
-
+public class Demo
+{
+  public static void main(String args[])
+  {
+    int arr[]={9,3,1,4,2,7,5};
+    int min,temp;
+    for(int i;i<arr.length;i++)
+    {
+      min=i;
+      for(int j=i+1;j<arr.length;j++)
+      {
+        if(arr[j]<arr[min];
+           {
+             min=j;
+           }
+      }
+       temp=arr[i];
+       arr[i]=arr[min];
+       arr[min]=temp;
+  }
+           for(int i:arr)
+           {
+             System.out.print(i+" ");
+           }
+           }
+           }
+           
+ // 1 2 3 4 5 7 9 
+           
+           
+  
