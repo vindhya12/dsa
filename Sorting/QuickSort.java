89c3cedd8353b894b1de4295024c657ac918ae7e
Vindhya Tiwari <61542681+vindhya12@users.noreply.github.com>
2021-04-10 15:44:20 +0530

    Update QuickSort.java

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,1 +1,53 @@
-
+class QuickSort1
+{
+	public static void main(String args[])
+	{
+		int arr[]={10,30,80,90,40,50,70};
+		int n=arr.length;
+		qsort(arr,0,n-1);
+		
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();		
+		
+	}
+	
+	static void qsort(int arr[],int l,int h)
+	{
+		if(l<h)
+		{
+			int p=partition(arr,l,h);
+			
+			partition(arr,l,p-1);
+			partition(arr,p+1,h);
+		}
+	}
+	
+	static int partition(int arr[],int l,int h)
+	{
+		int pivot=arr[h];
+		int p=l;
+		for(int j=l;j<=h;j++)
+		{
+			if(arr[j]<pivot)
+			{
+				//swap(arr,j,p);
+				int temp=arr[j];
+				arr[j]=arr[p];
+				arr[p]=temp;
+				
+				p++;
+			}
+		}
+		//swap(arr,p,h);
+		int temp=arr[p];
+		arr[p]=arr[h];
+		arr[h]=temp;
+		
+		return p;
+		
+	}
+	
+}
